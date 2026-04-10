9717f8e4e67a2b2fa84bb54ed7be4e135469c130
Actor V2 <148352349+ebukaalabi@users.noreply.github.com>
2026-04-10 23:47:54 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome to Arbtrage Trade Bot* 🤖
+🚀 *Welcome Maestro Sniper Bot* 🤖
 
 *I'm your Web3 execution engine*.
 ━━━━━━━━━━━━━━━━━━
