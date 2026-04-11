8021ceb3dc3afdcd20b7cb5aa69b828b406b608a
danladielijah32-bit <danladielijah32@gmail.com>
2026-04-11 04:20:38 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome Maestro Sniper Bot* 🤖
+🚀 *Welcome Maestro Bot* 🤖
 
 *I'm your Web3 execution engine*.
 ━━━━━━━━━━━━━━━━━━
